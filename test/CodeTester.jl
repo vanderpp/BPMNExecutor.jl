497220c4a03c0e53e67dbe0f997a96e6a74430ef
PietVANDERPAELT <piet.van.der.paelt@vub.be>
2026-09-14 13:37:32 +0200

    Generator Extension and Navigation

--- test/CodeTester.jl
+++ test/CodeTester.jl
@@ -1,18 +1,6 @@
 using BPMNExecutor
 
-function showNodeElements(nodeElements)
-    [begin println(typeof(el), " ", get_id(el)); end for el in nodeElements]
-end
-
-function parseBPMN()
-    "./src/diagram.bpmn" |> retrieveTopProcess |> exploreFlow #|> showNodeElements
-end
-
 parsed = parseBPMN()
-startEvent = findStartEvent(parsed)
 
-
-#someTask = parsed[1].subnodes[2]
-#print(taskGenerator(someTask))
-
-#print(resumableGenerator("subProcess1"))
+print(topLevelResumableGenerator(parsed))
+print(topScopeGenerator(parsed))
